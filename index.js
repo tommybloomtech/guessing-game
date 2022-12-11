f722b1bbf7567f54e3e001abd55d233049969645
Thomas Sorensen <112982801+tommybloomtech@users.noreply.github.com>
2022-12-11 14:40:50 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-let maximum = prompt("Enter the maximum number");
+let maximum = parseInt(prompt("Enter the maximum number"));
+while(!maximum) {
+    maximum = parseInt(prompt("Enter a valid number"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+
+let guess = parseInt(prompt("Enter your first guess"));
+let attempts = 1;
+
+while (parseInt(guess !== targetNum)) {
+    if(guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
+    }
+}
+if(guess === 'q') {
+    console.log("OK, you quit!");
+} else {
+    console.log("Congrats, you win!");
+    console.log(`You got it! It took you ${attempts} guesses`);
+}
